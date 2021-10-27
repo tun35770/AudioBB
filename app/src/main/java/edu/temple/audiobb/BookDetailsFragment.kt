b779fff3e966467d46abfc4cd255701f5932be57
tun35770 <tun35770@temple.edu>
2021-10-26 20:52:56 -0400

    Implemented BookDetailsFragment.kt

--- app/src/main/java/edu/temple/audiobb/BookDetailsFragment.kt
+++ app/src/main/java/edu/temple/audiobb/BookDetailsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 
 /**
@@ -15,7 +18,9 @@ import android.view.ViewGroup
 class BookDetailsFragment : Fragment() {
 
     lateinit private var bookViewModel: BookViewModel
-
+    lateinit private var titleTextView: TextView
+    lateinit private var authorTextView: TextView
+    lateinit private var layout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,23 @@ class BookDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false)
+        layout = inflater.inflate(R.layout.fragment_book_details, container, false)
+
+        return layout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bookViewModel = ViewModelProvider(requireActivity()).get(BookViewModel::class.java)
+
+        titleTextView = layout.findViewById(R.id.titleTextView)
+        authorTextView = layout.findViewById(R.id.authorTextView)
+
+        bookViewModel.getBook().observe(viewLifecycleOwner, Observer{it ->
+            titleTextView.text = it.title
+            authorTextView.text = it.author
+        })
     }
 
 }
