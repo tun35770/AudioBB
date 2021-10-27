3f7d7eb86a9e44d72c3b5691e27cb73843772300
tun35770 <tun35770@temple.edu>
2021-10-26 20:40:00 -0400

    Finished BookAdapter.kt

--- app/src/main/java/edu/temple/audiobb/BookAdapter.kt
+++ app/src/main/java/edu/temple/audiobb/BookAdapter.kt
@@ -19,7 +19,7 @@ class BookAdapter (val _context : Context, val _bookList : BookList, val ocl: Vi
 
         init {
             // Define click listener for the ViewHolder's View.
-            booklistViewRecycler = view.findViewById(R.id.booklistRecyclerView)
+            booklistViewRecycler = view.findViewById(R.id.recyclerLayout)
         }
     }
 
@@ -27,29 +27,25 @@ class BookAdapter (val _context : Context, val _bookList : BookList, val ocl: Vi
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): BookAdapter.ViewHolder {
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.recycler_element, viewGroup, false)
+            .inflate(R.layout.booklist_recycler_element, viewGroup, false)
 
         return ViewHolder(view)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ImageAdapter.ViewHolder, _position: Int) {
+    override fun onBindViewHolder(viewHolder: BookAdapter.ViewHolder, _position: Int) {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         val position = _position
-        viewHolder.imageViewRecycler.setImageResource(imageObjects[position].resourceId)
+        val titleTextView = viewHolder.booklistViewRecycler.findViewById<View>(R.id.titleTextView)
+        val authorTextView = viewHolder.booklistViewRecycler.findViewById<View>(R.id.authorTextView)
 
-        viewHolder.imageViewRecycler.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View?) {
-                imageViewModel.saveResourceId(imageObjects[position].resourceId)
-                imageViewModel.saveTitle(imageObjects[position].title)
-            }
-        })
+        viewHolder.booklistViewRecycler.setOnClickListener(ocl)
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount(): Int{ return imageObjects.size }
+    override fun getItemCount(): Int{ return bookList.size() }
 
 }
